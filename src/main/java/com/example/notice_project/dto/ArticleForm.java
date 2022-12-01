b54eb4fe62a958bb6f210f157f22bf22daa331d8
Eddie <heungryuncho@naver.com>
2022-12-01 18:05:50 +0900

    221201

--- src/main/java/com/example/notice_project/dto/ArticleForm.java
+++ src/main/java/com/example/notice_project/dto/ArticleForm.java
@@ -2,10 +2,14 @@ package com.example.notice_project.dto;
 
 import com.example.notice_project.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor // 이 어노테이션을 쓰면 Constructor, Setter 생성자 안써줘도 됨.
+@NoArgsConstructor
 @ToString
+@Setter
 public class ArticleForm {
 
     private Long id; // id 필드 추가
--- src/main/java/com/example/notice_project/entity/Article.java
+++ src/main/java/com/example/notice_project/entity/Article.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식하게 함!
+@Entity // DB가 해당 객체를 인식하게 함! (해당 클래스로 테이블을 만든다!)
 @AllArgsConstructor // 모든 필드에 있는 생성자를 만들어줌
 @NoArgsConstructor // 디폴트 생성자 추가
 @ToString
@@ -18,7 +15,7 @@ import javax.persistence.Id;
 public class Article {
 
     @Id //대푯갑을 지정 eg. 주민등록번호
-    @GeneratedValue // 1,2,3 자동 어노테이션!
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 1,2,3 자동 어노테이션!
     private Long id;
 
     @Column
@@ -27,4 +24,10 @@ public class Article {
     @Column
     private String content;
 
+    public void patch(Article article) {
+        if(article.title != null)
+            this.title = article.title;
+        if(article.content != null)
+            this.content = article.content;
+    }
 }
